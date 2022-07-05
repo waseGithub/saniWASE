1a6e9a474fe2a01ef9bae349387140164c2270f5
waseGithub <108359561+waseGithub@users.noreply.github.com>
2022-07-05 20:03:04 +0100

    Update checkpy.py

--- checkpy.py
+++ checkpy.py
@@ -3,6 +3,13 @@ import subprocess
   
 pytonProcess = subprocess.check_output("ps -ef | grep .py",shell=True).decode()
 pytonProcess = pytonProcess.split('\n')
-  
+
+found = False 
 for process in pytonProcess:
     print(process)
+    if process == 'wase      206225  206171  0 19:58 pts/1    00:00:00 python3 combined.py':
+      print('process found')
+      found = True
+      break
+if found == False:
+  print('data script not running')
