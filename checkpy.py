e08d446e4d0582754303e5258e0468a29e0387b8
waseGithub <108359561+waseGithub@users.noreply.github.com>
2022-07-05 19:57:56 +0100

    Create checkpy.py

---
+++ checkpy.py
@@ -0,0 +1,8 @@
+import subprocess
+  
+  
+pytonProcess = subprocess.check_output("ps -ef | grep .py",shell=True).decode()
+pytonProcess = pytonProcess.split('\n')
+  
+for process in pytonProcess:
+    print(process)
