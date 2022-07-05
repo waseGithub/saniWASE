c1ca098d2d8083f90d07ba85b305697b4895d364
waseGithub <108359561+waseGithub@users.noreply.github.com>
2022-07-05 20:20:53 +0100

    Update checkpy.py

--- checkpy.py
+++ checkpy.py
@@ -1,8 +1,13 @@
 import subprocess
+import smtplib
+from email.mime.text import MIMEText
   
   
 pytonProcess = subprocess.check_output("ps -ef | grep .py",shell=True).decode()
 pytonProcess = pytonProcess.split('\n')
+
+
+
 
 found = False 
 for process in pytonProcess:
@@ -13,3 +18,15 @@ for process in pytonProcess:
       break
 if found == False:
   print('data script not running')
+  with open(textfile, 'rb') as fp:
+      # Create a text/plain message
+      msg = MIMEText(fp.read())
+      msg['Subject'] = 'script:combined.py failure on systems:linux at site:saniwase_hepworth'
+      msg['From'] = harvey.rutland@wase.co.uk
+      msg['To'] = william.gambier@wase.co.uk
+
+      # Send the message via our own SMTP server, but don't include the
+      # envelope header.
+      s = smtplib.SMTP('localhost')
+      s.sendmail(me, [you], msg.as_string())
+      s.quit()
