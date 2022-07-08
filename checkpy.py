e8296effcc7cd4ee12d8c7540582ac3819c31b1a
waseGithub <108359561+waseGithub@users.noreply.github.com>
2022-07-08 10:55:49 +0100

    Update checkpy.py

--- checkpy.py
+++ checkpy.py
@@ -46,7 +46,8 @@ while(1):
     print('data script not running')
     if attempts <= 3:
 #       call(["python3", "combined.py"])
-      call(["gnome-terminal", "-x", "sh", "-c", "python3 combined.py"])
+#       call(["gnome-terminal", "-x", "sh", "-c", "python3 combined.py"])
+      call(["gnome-terminal", "-x", "python3 combined.py"])
       attempts +=1 
       print('attempts =')
       print(attempts)
