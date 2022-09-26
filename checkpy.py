9eda8a307c23ea43f29f2db4ea31ee185f48b478
waseGithub <108359561+waseGithub@users.noreply.github.com>
2022-09-26 19:12:33 +0100

    Update checkpy.py

--- checkpy.py
+++ checkpy.py
@@ -46,7 +46,7 @@ while(1):
     print('data script not running')
     if attempts <= 3:
       #call(["gnome-terminal",  "-x", "sh", "-c", "python3 combined.py"])
-      subprocess.Popen('python3 combined.py',shell = true, creationflags=CREATE_NEW_CONSOLE)
+      subprocess.Popen('python3 combined.py',shell = True, creationflags=CREATE_NEW_CONSOLE)
    
      
   
