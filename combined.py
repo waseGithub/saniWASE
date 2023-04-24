05e0fd8fa39c16c695a29e8c306a2a801295ee27
harveyrutland <hcrutland@mail.com>
2023-04-24 14:39:48 +0100

    flowmeter database test

--- combined.py
+++ combined.py
@@ -227,8 +227,8 @@ if __name__ == '__main__':
              curr = time.ctime(curr) 
              uploadfile1 = 'sensor_all' + '.csv'
              uploadfile2 = 'tank_data' + '.csv'
-             data_gas.to_csv(uploadfile1, 'w')
-             data_tank.to_csv(uploadfile2, 'w')
+             data_gas.to_csv(uploadfile1)
+             data_tank.to_csv(uploadfile2)
              
              
              subprocess.run(["python3", "pushtosql_flow.py"])
