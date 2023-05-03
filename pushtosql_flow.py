cb57f4540c7377fc8d63860610759e346936b6ce
harveyrutland <hcrutland@mail.com>
2023-05-03 09:32:24 +0100

    repush

--- pushtosql_flow.py
+++ pushtosql_flow.py
@@ -115,7 +115,7 @@ df_biogasflow = df_biogasflow.groupby(level='ID').resample('30T', level=0).max()
 print(df_biogasflow)
 df_biogasflow.reset_index(inplace=True)
 # df_biogasflow['datetime'] = df_biogasflow['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
-print('reset_datetrime')
+
 
 
 
